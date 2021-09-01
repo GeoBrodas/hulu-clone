9d0560b167ad511a9356fb93bee886255cb7c8f7
GeoBrodas <geobro2310@gmail.com>
2021-09-01 23:06:46 +0530

    complete responsive header

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,9 +1,27 @@
+import {
+  HomeIcon,
+  LightningBoltIcon,
+  BadgeCheckIcon,
+  CollectionIcon,
+  SearchIcon,
+  UserIcon,
+} from '@heroicons/react/outline';
+
 import Image from 'next/image';
+import HeaderItem from './HeaderItem';
 
 function Header() {
   return (
-    <header>
-      <h1>HULU</h1>
+    <header className="flex flex-col sm:flex-row m-5 justify-between items-center h-auto">
+      {/* flex-grow: grow and take the full width */}
+      <div className="flex flex-grow justify-evenly max-w-2xl">
+        <HeaderItem title="HOME" Icon={HomeIcon} />
+        <HeaderItem title="TRENDING" Icon={LightningBoltIcon} />
+        <HeaderItem title="VERIFIED" Icon={BadgeCheckIcon} />
+        <HeaderItem title="COLLECTION" Icon={CollectionIcon} />
+        <HeaderItem title="SEARCH" Icon={SearchIcon} />
+        <HeaderItem title="ACCOUNT" Icon={UserIcon} />
+      </div>
       <Image
         className="object-contain"
         alt="HULU"
